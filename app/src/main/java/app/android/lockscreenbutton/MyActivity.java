c73fdf83da7ef9611e641768ab7fbc533fdb6c30
Deepak Kumar <deepak@emjoyment.com>
2014-10-21 17:30:06 -0400

    Everything but lock screen done (widgets, etc..)

---
+++ app/src/main/java/app/android/lockscreenbutton/MyActivity.java
@@ -0,0 +1,79 @@
+package app.android.lockscreenbutton;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+
+public class MyActivity extends Activity {
+    private Button pressToPlay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Set up our Lockscreen
+        makeFullScreen();
+        startService(new Intent(this,LockScreenService.class));
+
+        setContentView(R.layout.activity_my);
+
+        pressToPlay = (Button) findViewById(R.id.press_to_play);
+    }
+
+    public void playOnClick(View view){
+        Log.e("Pressed","the btn");
+    }
+
+    /**
+     * A simple method that sets the screen to fullscreen.  It removes the Notifications bar,
+     *   the Actionbar and the virtual keys (if they are on the phone)
+     */
+    public void makeFullScreen() {
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        if(Build.VERSION.SDK_INT < 19) { //View.SYSTEM_UI_FLAG_IMMERSIVE is only on API 19+
+            this.getWindow().getDecorView()
+                    .setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        } else {
+            this.getWindow().getDecorView()
+                    .setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        return; //Do nothing!
+    }
+
+    public void unlockScreen(View view) {
+        //Instead of using finish(), this totally destroys the process
+        android.os.Process.killProcess(android.os.Process.myPid());
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
